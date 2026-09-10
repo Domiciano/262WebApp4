da0854e21b1a57515c8da98bced860dadf1adacf
Domiciano Rincón <domicianorincon@Domicianos-MacBook-Pro.local>
2026-09-10 11:09:16 -0500

    Semana 6

--- src/main/java/edu/co/icesi/introspringboot/repository/RolePermissionRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.co.icesi.introspringboot.repository;
-
-import edu.co.icesi.introspringboot.entity.RolePermission;
-import edu.co.icesi.introspringboot.entity.keys.RolePermissionId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RolePermissionRepository extends JpaRepository<RolePermission, RolePermissionId> {
-}
--- src/main/java/edu/co/icesi/introspringboot/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.co.icesi.introspringboot.repository;
-
-import edu.co.icesi.introspringboot.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
